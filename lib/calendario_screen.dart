a7a9e06dcea0ee0d749be8fae5a53fb2a1480907
azucenavazquez <karla29.vazquezl@gmail.com>
2024-10-30 21:51:05 -0600

    proyecto

---
+++ lib/calendario_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class CalendarScreen extends StatefulWidget {
+  @override
+  _CalendarScreenState createState() => _CalendarScreenState();
+}
+
+class _CalendarScreenState extends State<CalendarScreen> {
+  // Controlador para el calendario
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calendario'),
+      ),
+      body: Column(
+        children: [
+          TableCalendar(
+            firstDay: DateTime(2000), // Primer día disponible en el calendario
+            lastDay: DateTime(2100),  // Último día disponible en el calendario
+            focusedDay: _focusedDay,
+            calendarFormat: _calendarFormat,
+            selectedDayPredicate: (day) {
+              return isSameDay(_selectedDay, day);
+            },
+            onDaySelected: (selectedDay, focusedDay) {
+              setState(() {
+                _selectedDay = selectedDay;
+                _focusedDay = focusedDay; // Cambia el día enfocado
+              });
+            },
+            onFormatChanged: (format) {
+              if (_calendarFormat != format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              }
+            },
+            onPageChanged: (focusedDay) {
+              _focusedDay = focusedDay;
+            },
+          ),
+          SizedBox(height: 20),
+          if (_selectedDay != null) // Muestra la fecha seleccionada
+            Text(
+              'Fecha seleccionada: ${_selectedDay?.toLocal()}'.split(' ')[0],
+              style: TextStyle(fontSize: 18),
+            ),
+        ],
+      ),
+    );
+  }
+}
